f7a9995c3acf60a7e215f6f7c6b34ba950714679
Lingersky <wumengjin721@163.com>
2019-05-23 15:52:01 +0800

    Update Texas-Holdem.cpp
    add notes

--- Texas-Holdem.cpp
+++ Texas-Holdem.cpp
@@ -7,6 +7,13 @@ using namespace std;
 
 constexpr unsigned char Error = 0xff;
 
+
+/**********************************************************************************
+
+The special set values can help to get the type more quickly.
+
+**********************************************************************************/
+
 constexpr char ONE = 1;
 constexpr char TWO = 5;
 constexpr char THREE = 10;
@@ -29,19 +36,19 @@ struct cardType {
 }card[] = { {"Offsuit",OFFSUIT} ,{"Pair",PAIR},{"Two Pairs",TWOPAIR},{"Set",SET},{"Straight",STRAIGHT},
 					 {"Suited",SUITED},{"Full House",FULLHOUSE},{"Bomb",BOMB},{"Flush",FLUSH} };
 
-const char ascNum[13] = { '2','3','4','5','6','7','8','9','T','J','Q','K','A' };
-const char ascSuit[4] = { 'C','D','H','S' };
+const char ascNum[13] = { '2','3','4','5','6','7','8','9','T','J','Q','K','A' };//card num charatcer (forward order)
+const char ascSuit[4] = { 'C','D','H','S' };//pattern character
 
 struct compareList {
-	char type = 0;
-	char list[5] = { 0 };
+	char type = 0;//memorize the type, it will be compared at first
+	char list[5] = { 0 };//memorize the card values, until the type is the same, it will not be compared
 };
 
 struct sortList {
 	char pairCount = 0;
-	char pairNum[2] = { 0 };
-	char uniqueNum = 0;
-	char offNum[5] = { 0 };
+	char pairNum[2] = { 0 };//memorize the pairs' values
+	char uniqueNum = 0;//memorize the set or bomb value
+	char offNum[5] = { 0 };//memorize the singles' values
 };
 
 unsigned char asc_search(char num, char* list)
@@ -52,7 +59,7 @@ unsigned char asc_search(char num, char* list)
 	return Error;
 }
 
-char straight_check(char* straight)
+char straight_check(char* straight)//check the straight
 {
 	for (char i = 0; i < 4; ++i)
 		if (*(straight + i) - *(straight + i + 1) != 1)
@@ -60,7 +67,7 @@ char straight_check(char* straight)
 	return true;
 }
 
-compareList type_check(char* cardCount)
+compareList type_check(char* cardCount)//with the array of cardCount, we can easily get the card type
 {
 	compareList result;
 	sortList sort;
@@ -69,37 +76,37 @@ compareList type_check(char* cardCount)
 	for (i = 12; i >= 0 && count < 5; i--)
 	{
 		switch (cardCount[i]) {
-		case 3:
+		case 3://A single card
 			sort.offNum[count] = i;
-			++count;
-			type += ONE;
+			++count;//only one card
+			type += ONE;// the core of quickly get the type while tranversing the array
 			break;
-		case 2:
+		case 2://find a pair
 			sort.pairNum[sort.pairCount] = i;
 			++sort.pairCount;
-			count += 2;
+			count += 2;// a pair has two cards
 			type += TWO;
 			break;
-		case 1:
+		case 1:// find a set
 			sort.uniqueNum = i;
 			count += 3;
 			type += THREE;
 			break;
-		case 0:
+		case 0:// find a bomb
 			sort.uniqueNum = i;
 			count += 4;
 			type += FOUR;
 		}
 	}
-	switch (type) {
+	switch (type) {//with the special value, the different type of card can be accumulated into a final type
 	case OFFSUIT:
-		if (straight_check(sort.offNum))
+		if (straight_check(sort.offNum))//5 single cards can be a Straight
 			type = STRAIGHT;
 		memcpy(result.list, sort.offNum, 5);
 		break;
 	case PAIR:
-		result.list[0] = *(sort.pairNum);
-		memcpy(result.list + 1, sort.offNum, 3);
+		result.list[0] = *(sort.pairNum);//memorize the pair value, it will be firstly compared
+		memcpy(result.list + 1, sort.offNum, 3);//memorize the single card
 		break;
 	case TWOPAIR:
 		memcpy(result.list, sort.pairNum, 2);
@@ -108,7 +115,7 @@ compareList type_check(char* cardCount)
 	case SET:
 	case FULLHOUSE:
 	case BOMB:
-		result.list[0] = sort.uniqueNum;
+		result.list[0] = sort.uniqueNum;//only need to compare one value
 	}
 	result.type = type;
 	return result;
@@ -116,28 +123,28 @@ compareList type_check(char* cardCount)
 
 char card_revert(char* card, compareList* outList)
 {
-	char cardCount[13] = { 4,4,4,4,4,4,4,4,4,4,4,4,4 };
+	char cardCount[13] = { 4,4,4,4,4,4,4,4,4,4,4,4,4 };//Corresponding to array ascNum
 	char cardType = 0;
 	bool suited = true;
 	unsigned char num;
 
-	for (char i = 0; i < 5; ++i)
+	for (char i = 0; i < 5; ++i)// Traversing the input string
 	{
-		num = asc_search(card[i * 3], (char*)ascNum);
+		num = asc_search(card[i * 3], (char*)ascNum);//find the palce of current character in array ascNum
 		if (num == Error && asc_search(card[i * 3 + 1], (char*)ascSuit) == Error)
 			return false;
-		cardCount[num]--;
-		if (!i)
+		cardCount[num]--;//for example: if '4' is find once, the cardCount[2] decreases 1
+		if (!i)//memorize the first pattern
 		{
 			cardType = card[1];
 			continue;
 		}
-		if (suited && (cardType - card[i * 3 + 1]))
-			suited = false;
+		if (suited && (cardType - card[i * 3 + 1]))//compare the current pattern with the previous pattern
+			suited = false;//if patters are not the same, it couldn't be a Suited.
 	}
 	*outList = type_check(cardCount);
-	if (suited&&outList->type <= STRAIGHT)
-		if (outList->type == STRAIGHT)
+	if (suited&&outList->type <= STRAIGHT)//Suited is a higher type of card
+		if (outList->type == STRAIGHT)//special for Straight
 			outList->type = FLUSH;
 		else
 			outList->type = SUITED;
@@ -161,7 +168,7 @@ void texas_holdem(void)
 	memcpy(stringBlack, string + 7, 14);
 	memcpy(stringWhite, string + 29, 14);
 	free(string);
-	card_revert(stringBlack, &listBlack);
+	card_revert(stringBlack, &listBlack);//get the type
 	card_revert(stringWhite, &listWhite);
 	char* winstring = (char*)malloc(15 * sizeof(char));;
 	char wintype = 0;
@@ -169,29 +176,29 @@ void texas_holdem(void)
 	char i = 0;
 	if (listBlack.type != listWhite.type)
 	{
-		wintype = listBlack.type > listWhite.type ? listBlack.type : listWhite.type;
+		wintype = listBlack.type > listWhite.type ? listBlack.type : listWhite.type;//compare the type
 		winner = listBlack.type > listWhite.type ? true : false;
-		while (wintype != card[i].value)
+		while (wintype != card[i].value)//find the type name
 			++i;
 		memcpy(winstring, card[i].name, 15);
 	}
 	else
 	{
-		for (i = 0; i < 5; i++)
+		for (i = 0; i < 5; i++)//compare the card while have the same type
 			if (listBlack.list[i] != listWhite.list[i])
 			{
 				winner = listBlack.list[i] > listWhite.list[i] ? true : false;
-				sprintf_s(winstring, 12, "high card:%c", listBlack.list[i] > listWhite.list[i] ? ascNum[listBlack.list[i]] : ascNum[listWhite.list[i]]);
+				sprintf_s(winstring, 12, "high card:%c", listBlack.list[i] > listWhite.list[i] ? ascNum[listBlack.list[i]] : ascNum[listWhite.list[i]]);//matched the high card
 				break;
 			}
-		if (i == 5)
+		if (i == 5)//a draw
 		{
 			printf("Tie\r\n");
 			free(winstring);
 			return;
 		}
 	}
-	printf(winner ? "Black wins - %s\r\n" : "White wins - %s\r\n", winstring);
+	printf(winner ? "Black wins - %s\r\n" : "White wins - %s\r\n", winstring);// print the result
 	free(winstring);
 	return;
 }
